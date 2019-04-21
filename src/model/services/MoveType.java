55c4cc419bd385691c6e53225f69703ae9d82647
Impressive Now <thomas.coggia@gmail.com>
2019-04-22 01:56:47 +0200

    + Opposite MoveType

--- src/model/services/MoveType.java
+++ src/model/services/MoveType.java
@@ -6,5 +6,26 @@ public enum MoveType
 	RIGHT,
 	DOWN,
 	UP,
-	NEUTRAL
+	NEUTRAL;
+	
+	public MoveType opposite()
+	{
+		switch(this)
+		{
+		case LEFT:
+			return RIGHT;
+		case RIGHT:
+			return LEFT;
+		case DOWN:
+			return UP;
+		case UP:
+			return DOWN;
+		case NEUTRAL:
+			return NEUTRAL;
+		default:
+			break;
+		}
+		assert false;
+		return MoveType.NEUTRAL;
+	}
 }
