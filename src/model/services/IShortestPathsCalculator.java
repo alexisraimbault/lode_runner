571cadb9881aac38b6bea7f2a132fba1342ae225
Impressive Now <thomas.coggia@gmail.com>
2019-04-22 20:38:43 +0200

    Ajout AStarDecision (suppr)

--- src/model/services/IShortestPathsCalculator.java
+++
@@ -1,9 +0,0 @@
-package model.services;
-
-import java.util.List;
-import java.util.Set;
-
-public interface IShortestPathsCalculator
-{
-	List<MoveType> getPaths(ICharacter character, ICharacterMoveAccepter accepter);
-}
